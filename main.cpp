866b5ecf4da56809bd752ebd137766ba4e854187
AlexandruPM <104488066+AlexandruPM@users.noreply.github.com>
2022-05-05 18:24:07 +0300

    problema 4

--- main.cpp
+++ main.cpp
@@ -1,22 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    ifstream f("note.in");
-    int n,m,i,j,a[100][100],v[100],s,minim,maxim;
-    cin>>n>>m;
-    minim=9999;
-    maxim=-9999;
-    p=0;
-    for(i=0; i<=n-1; i++)
-        for(j=0; j<=m-1; j++)
-            f>>a[i][j];
-    for(i=0; i<=n-1; i++)
-        s=0;
-    for(j=0; j<=m-1; j++){
-            if(a[i][j]<minim) minim=a[i][j];
-    if(a[i][j]>maxim) maxim=a[i][j];
+    int n,i,v[100];
+    cin>>n;
+    for(i=0;i<=n-1;i++)
+        cin>>v[i];
+    for(i=0;i<=n-1;i++)
+        while(v[i])
+    {
+        cif=v[i]%10;
+        nr++;
+
     }
 }
