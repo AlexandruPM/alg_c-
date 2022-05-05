39651e7bb234f521869046f9bd2c94fb52ee7ac1
AlexandruPM <104488066+AlexandruPM@users.noreply.github.com>
2022-05-05 17:16:57 +0300

    problema 3

--- main.cpp
+++ main.cpp
@@ -1,31 +1,22 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    int n,i,j,v[1001],a[1001],ok=1;
-    cin>>n;
-    for(i=1; i<=n; i++)
-        cin>>v[i];
-    for(i=1; i<=n; i++)
-    {
-        cin>>a[i];
-        for(j=1; j<=n; j++)
-            if(v[j]==a[i])
-            {
-                v[j]=-1;
-                j=n+1;
-            }
+    ifstream f("note.in");
+    int n,m,i,j,a[100][100],v[100],s,minim,maxim;
+    cin>>n>>m;
+    minim=9999;
+    maxim=-9999;
+    p=0;
+    for(i=0; i<=n-1; i++)
+        for(j=0; j<=m-1; j++)
+            f>>a[i][j];
+    for(i=0; i<=n-1; i++)
+        s=0;
+    for(j=0; j<=m-1; j++){
+            if(a[i][j]<minim) minim=a[i][j];
+    if(a[i][j]>maxim) maxim=a[i][j];
     }
-    for(i=1; i<=n; i++)
-        if(v[i]!=-1)
-        {
-            ok=0;
-            i=n+1;
-        }
-    if(ok==1)
-        cout<<"DA";
-    else
-        cout<<"NU";
 }
